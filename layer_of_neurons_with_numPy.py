d81107c3991081a9bcb4b36cbaa76f325ab8415e
Frazer Hogg <frazerj@me.com>
2021-08-30 09:56:35 +0100

    Task 5: Layer of neurons with numpy

---
+++ layer_of_neurons_with_numPy.py
@@ -0,0 +1,25 @@
+'''
+** A layer of neurons with NumPy **
+
+An example of a layer of neurons, using the dot product and the addition 
+of the vectors with NumPy
+
+https://nnfs.io/cyx 
+
+'''
+
+import numpy as np
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+
+weights = [
+    [0.2, 0.8, -0.5,1],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+]
+
+biases = [2.0, 3.0, 0.5]
+
+layer_outputs = np.dot(weights, inputs) + biases
+
+print(layer_outputs)
