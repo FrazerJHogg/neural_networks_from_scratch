19f83d6e4aff4ca563d3a9666a1d81a4eddf462e
Frazer Hogg <frazerj@me.com>
2021-08-31 15:21:10 +0100

    Added example code for calculating accuracy

---
+++ example_code/accuracy.py
@@ -0,0 +1,30 @@
+'''
+
+** Accuracy ** 
+
+Example code showing how to calculate accuracy. 
+
+'''
+
+import numpy as np 
+
+# Probabilities of 3 samples 
+softmax_outputs = np.array([
+    [0.7, 0.2, 0.1],
+    [0.5, 0.1, 0.4],
+    [0.02, 0.9, 0.08]
+])
+
+# Target (ground-truth) labels for 3 samples
+class_targets = np.array([0, 1, 1])
+
+# Calculate values along the second axis (axis of index 1 - columns)
+predictions = np.argmax(softmax_outputs, axis=1)
+
+# If the targets are one-hot encoded - convert them
+if len(class_targets.shape) == 2:
+    class_targets = np.argmax(class_targets, axis=1)
+# True evaluates to 1; False to 0
+accuracy = np.mean(predictions == class_targets)
+
+print('acc: ', accuracy)
