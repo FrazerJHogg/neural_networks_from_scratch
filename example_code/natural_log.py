55566ff13037e6fbd213a29cc0e31a17449aeb38
Frazer Hogg <frazerj@me.com>
2021-08-31 10:57:13 +0100

    Added example of natural log

---
+++ example_code/natural_log.py
@@ -0,0 +1,9 @@
+import numpy as np
+import math
+
+# Natural log using Euler's number (~2.71828)
+b = 5.2
+print(np.log(b))
+
+# Confirm result through exponentiating 
+print(math.e ** 1.6486586255873816)
